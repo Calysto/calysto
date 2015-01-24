f621bed2930bfdde5e7b6402508ac856a087fa4e
Doug Blank <dblank@cs.brynmawr.edu>
2015-01-23 19:15:39 -0500

    Version 0.6.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if svem_flag in sys.argv:
     sys.argv.remove(svem_flag)
 
 setup(name='calysto',
-      version='0.2',
+      version='0.6.0',
       description='Libraries and Languages for Python and IPython',
       long_description="Libraries and Languages for IPython and Python",
       author='Douglas Blank',
